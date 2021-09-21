332504322b453e1ab7e22391e46cdcecdd5e233d
Ibraheem <wahabibraheemiyanda@gmail.com>
2021-09-21 20:25:23 +0100

    update please

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crypt/crypt.dart';
 import 'package:flutter/material.dart';
+
 class Login extends StatefulWidget {
-  const Login({ Key? key }) : super(key: key);
+  const Login({Key? key}) : super(key: key);
 
   @override
   _LoginState createState() => _LoginState();
@@ -8,162 +11,201 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
+  TextEditingController _matricNo = TextEditingController();
+  TextEditingController _password = TextEditingController();
   @override
   // SingleChildScrollView(
-    //   scrollDirection: Axis.vertical,
-      // child: 
+  //   scrollDirection: Axis.vertical,
+  // child:
   Widget build(BuildContext context) {
-    return  Scaffold(
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+    return Scaffold(
       body: Container(
-           height: double.infinity,
-            width: double.infinity,
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/background.jpg"), fit: BoxFit.fill)),
+        child: Center(
+          child: Container(
+            width: 350.0,
+            height: 900,
+            padding: EdgeInsets.fromLTRB(10, 2, 10, 5),
+            margin: EdgeInsets.fromLTRB(30, 100, 20, 50),
             decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/background.jpg"), fit: BoxFit.fill)),
-            child: Center(
-              child: Container(
-                width: 350.0,
-                height: 900,
-                 padding: EdgeInsets.fromLTRB(10, 2, 10, 5),
-                    margin: EdgeInsets.fromLTRB(30, 100, 20, 50),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      // border: BoxBorder(2.0),
+              color: Colors.white,
+              // border: BoxBorder(2.0),
+            ),
+            child: ListView(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      height: 150,
+                      width: 200,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                        image: AssetImage("assets/logo_login.png"),
+                        alignment: Alignment.center,
+                        fit: BoxFit.contain,
+                      )),
                     ),
-                    child: ListView(
-                      children: [
-                        Column(
-                          children: [
-                            Container(
-                              height: 150,
-                              width: 200,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage("assets/logo_login.png"),
-                                   alignment: Alignment.center,
-                                    fit: BoxFit.contain,
-                                  )
+                    Container(
+                      margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      child: Form(
+                          key: _formKey,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              TextFormField(
+                                controller: _matricNo,
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.red,
+                                      width: 3.0,
+                                    ),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(0)),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.red,
+                                      width: 2.0,
+                                    ),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(50)),
+                                  ),
+                                  hoverColor: Colors.amber,
+                                  prefixIcon: const Icon(Icons.person),
+                                  hintText: 'Enter your Matric Number',
+                                  labelText: 'Login ID',
+                                  labelStyle: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return 'Please enter your Login ID';
+                                  }
+                                  return null;
+                                },
                               ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                              child: Form(
-                                
-                                 key: _formKey,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                   mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    TextFormField(
-                                      
-                                       decoration: const InputDecoration(
-                                          border: OutlineInputBorder(),
-                                         focusedBorder: OutlineInputBorder(
-                                           borderSide: BorderSide(color: Colors.red, width: 3.0,),
-                                           borderRadius: BorderRadius.all(Radius.circular(0)),
-                                         ),
-                                         enabledBorder: OutlineInputBorder(
-                                           borderSide: BorderSide(color: Colors.red,  width: 2.0,),
-                                           borderRadius: BorderRadius.all(Radius.circular(50)),
-                                         ),
-                                        hoverColor: Colors.amber,
-                                         prefixIcon: const Icon(Icons.person),
-                                        hintText: 'Enter your Matric Number',
-                                        labelText: 'Login ID',
-                                        labelStyle: TextStyle(
-                                          color: Colors.blue,
-                                          fontSize: 20,
-                                        ),
-                                      ),
-                                      validator: (value){
-                                        if (value!.isEmpty){
-                                           return 'Please enter your Login ID';
-                                        }
-                                        return null;
-      
-                                      },
+                              SizedBox(
+                                height: 30,
+                              ),
+                              TextFormField(
+                                controller: _password,
+                                obscureText: true,
+                                obscuringCharacter: ("*"),
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.red,
+                                      width: 3.0,
                                     ),
-                                    SizedBox(
-                                      height: 30,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(0)),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.red,
+                                      width: 2.0,
                                     ),
-                                    TextFormField(
-                                       obscureText: true,
-                                      obscuringCharacter: ("*"),
-                                       decoration: const InputDecoration(
-                                         border: OutlineInputBorder(),
-                                         focusedBorder: OutlineInputBorder(
-                                           borderSide: BorderSide(color: Colors.red,  width: 3.0,),
-                                           borderRadius: BorderRadius.all(Radius.circular(0)),
-                                         ),
-                                         enabledBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(color: Colors.red,  width: 2.0,),
-                                           borderRadius: BorderRadius.all(Radius.circular(50)),
-                                         ),
-                                         prefixIcon: Icon(Icons.password),
-                                        hintText: 'Write your password',
-                                        labelText: 'Password',
-                                        labelStyle: TextStyle(
-                                          color: Colors.blue,
-                                          fontSize: 20,
-                                        ),
-                                      ),
-                                      validator: (value){
-                                        if (value!.isEmpty){
-                                           return 'Please enter your Password';
-                                        }
-                                        
-                                        return null;
-      
-                                      },
-                                    ),
-                                    SizedBox(
-                                      height: 10,
-                                    ),
-                                    Container(
-                                      alignment: Alignment.topRight,
-                                      child: TextButton(
-                                        onPressed: (){
-                                          Navigator.pushNamed(context, '/forget');
-                                        }, 
-                                      child: Text("forgot your password?", 
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(50)),
+                                  ),
+                                  prefixIcon: Icon(Icons.password),
+                                  hintText: 'Write your password',
+                                  labelText: 'Password',
+                                  labelStyle: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return 'Please enter your Password';
+                                  }
+
+                                  return null;
+                                },
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Container(
+                                alignment: Alignment.topRight,
+                                child: TextButton(
+                                    onPressed: () {
+                                      //Navigator.pushNamed(context, '/forget');
+                                      final c1 = Crypt.sha256('einstein');
+                                      print(c1);
+                                    },
+                                    child: Text(
+                                      "forgot your password?",
                                       textAlign: TextAlign.right,
                                       style: TextStyle(
-                                        
-                                            color: Colors.black, fontSize: 17, 
+                                        color: Colors.black,
+                                        fontSize: 17,
                                       ),
-                                      )
-                                      ),
-                                    ),
-                                     SizedBox(
-                                      height: 10,
-                                    ),
-                                    Container(
-                                      width: 300,
-                                      height: 70,
-                                      color: Colors.red,
-                                      child: TextButton(
-                                        onPressed: (){
-                                        //  if (_formKey.currentState!.validate()) {
-                                        //   Navigator.pushNamed(context, '/dash');
-                                        //  }
-                                           Navigator.pushNamed(context, '/dash');
-                                        }, 
-                                        child: Text(
-                                          "Log In", style: TextStyle(color: Colors.white, fontSize: 30),
-                                        )
-                                        ),
-                                    ),
-                                  ],
-                                )
-                                ),
-                            ),
-                          ],
-                        ),
-                      ],
+                                    )),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Container(
+                                width: 300,
+                                height: 75,
+                                color: Colors.red,
+                                child: TextButton(
+                                    onPressed: () async {
+                                      if (_formKey.currentState!.validate()) {
+                                        final response = await users
+                                            .doc(_matricNo.text
+                                                .replaceAll("/", ''))
+                                            .get();
+                                        print(response.data().toString());
+                                        if (response.exists) {
+                                          print("User Exist");
+                                          final hashedPassword =
+                                              Crypt(response.get("password"));
+                                          if (hashedPassword
+                                              .match(_password.text)) {
+                                            print("Correct Password");
+                                            Navigator.pushNamed(
+                                                context, '/dash');
+                                          } else {
+                                            print("Wrong Password");
+                                          }
+                                        } else {
+                                          print("Matric No not in db");
+                                        }
+                                        //Navigator.pushNamed(context, '/dash');
+                                      }
+
+                                      // Navigator.pushNamed(context, '/dash');
+                                    },
+                                    child: Text(
+                                      "Log In",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 30),
+                                    )),
+                              ),
+                            ],
+                          )),
                     ),
-              ),
+                  ],
+                ),
+              ],
             ),
+          ),
         ),
+      ),
     );
     // );
   }
